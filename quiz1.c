846de2f7807f55726fac7221f4d40f89066fad21
Kerem Erkut Çiftlikçi <76708644+KeremErkut@users.noreply.github.com>
2024-11-21 15:06:47 +0300

    Update quiz1.c

--- quiz1.c
+++ quiz1.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int main(){
+    char name1[50];
+    char name2[50];
+    printf("Enter your first name.");
+    scanf("%s", name1);
+    printf("Enter your second name.");
+    scanf("%s", name2);
+    
+    for(int i = 0; name1[i] != '\0'; i++){
+        printf("%c\n", name1[i]);
+    }
 
+    for(int i = 0; name2[i] != '\0'; i++){
+        printf("%c ", name2[i]);
+    }
+    
+}
