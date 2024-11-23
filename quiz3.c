e790bc0a98687829b2448d4b872a118d6c08b2a1
Kerem Erkut Çiftlikçi <76708644+KeremErkut@users.noreply.github.com>
2024-11-23 13:52:45 +0000

    update quiz3.c

--- quiz3.c
+++ quiz3.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+int year;
+int takeYear(){
+   
+    printf("Enter a year for check leap or not : \n");
+    if(scanf("%i",&year) != 1){
+        printf("ERROR : Invalid input.");
+        return 1;
+    }else{
+        return year;
+    }
+     
+}
 
+void isLeap(int year){
+    if((year % 4) == 0){
+        printf("This is leap year.");
+    }else{
+        printf("This is not a leap year.");
+    }
+
+    
+}
+
+
+int main(void){
+    takeYear();
+    isLeap(year);
+}
